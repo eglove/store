e02098e5510bf4d223f84027f92a8226f1552c77
Ethan <hello@ethang.email>
2024-09-20 23:05:31 -0500

    types missing

--- build.mjs
+++ build.mjs
@@ -3,7 +3,7 @@ import { projectBuilder } from "@ethang/project-builder/project-builder.js";
 await projectBuilder("store", "master", {
   isLibrary: true,
   publishDirectory: "dist",
-  scripts: ["UPDATE", "DEDUPE", "LINT"],
+  scripts: [],
   tsConfigOverrides: {
     compilerOptions: {
       emitDeclarationOnly: true,
