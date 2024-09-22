4c92002cd323a977e898f49d9b0f47788677383f
Ethan <hello@ethang.email>
2024-09-22 17:55:02 -0500

    add explicit return types

--- src/util.ts
+++ src/util.ts
@@ -2,13 +2,13 @@ import type { Listener, Store } from "./index";
 
 export const storeSubscriptionHandler = <T,>(
   store: Store<T>,
-) => {
+): (listener: Listener<T>) => void => {
   return (listener: Listener<T>) => {
     return store.subscribe(listener);
   };
 };
 
-export const storeSnapshotHandler = <T,>(store: Store<T>) => {
+export const storeSnapshotHandler = <T,>(store: Store<T>): () => T => {
   return () => {
     return store.get();
   };
